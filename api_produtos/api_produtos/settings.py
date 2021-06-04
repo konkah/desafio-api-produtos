20eda5379bac76c885e4c504886eca76bbb072c0
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 20:19:26 -0300

    fix static url

--- api_produtos/api_produtos/settings.py
+++ api_produtos/api_produtos/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/produtos_static/'
 LOGIN_REDIRECT_URL = '/'
