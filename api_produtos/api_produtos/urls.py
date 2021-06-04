fe744530356c24bbf771066442d25cc4f702bb15
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 20:17:56 -0300

    fix auth and admin paths

--- api_produtos/api_produtos/urls.py
+++ api_produtos/api_produtos/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('produtos_admin/', admin.site.urls),
+    path('produtos_auth/', include('rest_framework.urls')),
     path('', include('api.urls')),
 ]
